c3050f9230fa3c2c3a55dfe94a00b72e74d42d1f
Jack <bexhetbushi89@gmail.com>
2025-01-02 18:17:27 +0100

    git init

--- src/main/java/com/carrentalservice/carrentalservice/Entities/RentalCMP.java
+++ src/main/java/com/carrentalservice/carrentalservice/Entities/RentalCMP.java
@@ -14,5 +14,45 @@ public class RentalCMP {
     private String domain;
     private String contactAddress;
     private String owner;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDomain() {
+        return domain;
+    }
+
+    public void setDomain(String domain) {
+        this.domain = domain;
+    }
+
+    public String getContactAddress() {
+        return contactAddress;
+    }
+
+    public void setContactAddress(String contactAddress) {
+        this.contactAddress = contactAddress;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
 }
 
