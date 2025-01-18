fe0e5f7f6632641e01db57a1ec868adb0e7a5207
Jack <bexhetbushi89@gmail.com>
2025-01-18 14:39:43 +0100

    Fixed CarEntity

--- src/main/java/com/carrentalservice/carrentalservice/repositories/CarRepository.java
+++ src/main/java/com/carrentalservice/carrentalservice/repositories/CarRepository.java
@@ -1,4 +1,8 @@
 package com.carrentalservice.carrentalservice.repositories;
 
-public interface CarRepository {
+import com.carrentalservice.carrentalservice.entities.Car;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CarRepository extends JpaRepository<Car, Long> {
 }
