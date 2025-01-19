ccc7268d36f48f1685c57440cc77b429ba97f485
Jack <bexhetbushi89@gmail.com>
2025-01-19 13:09:20 +0100

    ...

--- src/main/java/com/carrentalservice/carrentalservice/controllers/CarController.java
+++ src/main/java/com/carrentalservice/carrentalservice/controllers/CarController.java
@@ -1,46 +1,23 @@
 package com.carrentalservice.carrentalservice.controllers;
 
+import com.carrentalservice.carrentalservice.dto.CarDto;
 import com.carrentalservice.carrentalservice.entities.Car;
 import com.carrentalservice.carrentalservice.service.CarService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/cars")
 public class CarController {
 
-    @Autowired
-    private CarService carService;
+    private final CarService carService;
 
-    // Get all cars
-    @GetMapping
-    public List<Car> getAllCars() {
-        return carService.getAllCars();
+    public CarController(CarService carService) {
+        this.carService = carService;
     }
 
-    // Create a new car
-    @PostMapping
-    public Car createCar(@RequestBody Car car) {
-        return carService.createCar(car);
-    }
-
-    // Get car by ID
-    @GetMapping("/{id}")
-    public Car getCarById(@PathVariable Long id) {
-        return carService.getCarById(id);
-    }
-
-    // Update car
-    @PutMapping("/{id}")
-    public Car updateCar(@PathVariable Long id, @RequestBody Car car) {
-        return carService.updateCar(id, car);
-    }
-
-    // Delete car
-    @DeleteMapping("/{id}")
-    public void deleteCar(@PathVariable Long id) {
-        carService.deleteCar(id);
+    @PutMapping("/update")
+    public Car updateCarMileageAndAmount(@RequestBody CarDto carDetails) {
+        return carService.updateCarMileageAndAmount(carDetails);
     }
 }
--- src/main/java/com/carrentalservice/carrentalservice/repositories/CarRepository.java
+++ src/main/java/com/carrentalservice/carrentalservice/repositories/CarRepository.java
@@ -3,6 +3,9 @@ package com.carrentalservice.carrentalservice.repositories;
 import com.carrentalservice.carrentalservice.entities.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 
 public interface CarRepository extends JpaRepository<Car, Long> {
+    List<Car> findAll(String title);
 }
