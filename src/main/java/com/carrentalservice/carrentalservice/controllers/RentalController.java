88c12107560c64d3dab4223ee55fa4d570334e48
Llazar Syziu <llazarsyziu62@gmail.com>
2025-01-19 15:36:48 +0100

    Fix after merge Kam plotesuar klasen RentalService RentalController

--- src/main/java/com/carrentalservice/carrentalservice/controllers/RentalController.java
+++ src/main/java/com/carrentalservice/carrentalservice/controllers/RentalController.java
@@ -1,4 +1,45 @@
 package com.carrentalservice.carrentalservice.controllers;
 
+import com.carrentalservice.carrentalservice.entities.Rental;
+import com.carrentalservice.carrentalservice.service.RentalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class RentalController {
+    private final RentalService rentalService;
+    @Autowired
+    public RentalController(RentalService rentalService) {
+        this.rentalService = rentalService;
+    }
+    @GetMapping
+    public List<Rental> getAllRentals() {
+        return rentalService.getAllRentals();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Rental> getRentalById(@PathVariable Long id) {
+        return rentalService.getRentalById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public Rental createRental(@RequestBody Rental rental) {
+        return rentalService.saveRental(rental);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Rental> updateRental(@PathVariable Long id, @RequestBody Rental rentalDetails) {
+        return rentalService.getRentalById(id)
+                .map(existingRental -> {
+                    existingRental.setName(rentalDetails.getName());
+                    existingRental.setInternetDomain(rentalDetails.getInternetDomain());
+                    existingRental.setContactAddress(rentalDetails.getContactAddress());
+                    existingRental.setOwner(rentalDetails.getOwner());
+                    existingRental.setLogotype(rentalDetails.getLogotype());
+                    return ResponseEntity.ok(rentalService.saveRental(existingRental));
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/com/carrentalservice/carrentalservice/service/RentalService.java
+++ src/main/java/com/carrentalservice/carrentalservice/service/RentalService.java
@@ -1,4 +1,27 @@
 package com.carrentalservice.carrentalservice.service;
 
+import com.carrentalservice.carrentalservice.entities.Rental;
+import com.carrentalservice.carrentalservice.repositories.RentalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
 public class RentalService {
+
+    private final RentalRepository rentalRepository;
+    @Autowired
+    public RentalService(RentalRepository rentalRepository) {
+        this.rentalRepository = rentalRepository;
+    }
+    public List<Rental> getAllRentals() {
+        return rentalRepository.findAll();
+    }
+    public Optional<Rental> getRentalById(Long id) {
+        return rentalRepository.findById(id);
+    }
+    public Rental saveRental(Rental rental) {
+        return rentalRepository.save(rental);
+    }
+
 }
