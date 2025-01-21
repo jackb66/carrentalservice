2b449dd227e8f24d8646a247c7c150db3a17c282
Greidi Zykaj <greidizykaj@gmail.com>
2025-01-21 12:48:59 +0100

    Merge remote-tracking branch 'origin/Llazar'-s-branch' into Greidi's-branch
    # Conflicts:
    #	src/main/java/com/carrentalservice/carrentalservice/service/BranchService.java

---
+++ src/main/java/com/carrentalservice/carrentalservice/security/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.carrentalservice.carrentalservice.security;
+
+import com.carrentalservice.carrentalservice.entities.Employee;
+
+import com.carrentalservice.carrentalservice.repositories.EmployeeRepository;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public UserDetailServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Employee user = employeeRepository.findByUsername(username).orElseThrow();
+        return new UserDetailsImpl(user);
+
+    }
+}
---
+++ src/main/java/com/carrentalservice/carrentalservice/security/UserDetailsImpl.java
@@ -0,0 +1,61 @@
+package com.carrentalservice.carrentalservice.security;
+
+import com.carrentalservice.carrentalservice.entities.Employee;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserDetailsImpl implements UserDetails {
+
+
+    private String username;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> authorities;
+
+    public UserDetailsImpl(Employee user) {
+        this.username = user.getUsername();
+        this.password = user.getPassword();
+        this.active = user.getIsActive();
+        this.authorities = List.of(new SimpleGrantedAuthority(user.getPosition().name()));
+
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return active;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return active;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return active;
+    }
+}
