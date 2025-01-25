df8ff0c1f72c06c6d4dd7701ba4ace82aaf7bb00
Greidi Zykaj <greidizykaj@gmail.com>
2025-01-25 10:06:28 +0100

    calculate revenue

--- src/main/java/com/carrentalservice/carrentalservice/service/RevenueService.java
+++ src/main/java/com/carrentalservice/carrentalservice/service/RevenueService.java
@@ -1,24 +1,12 @@
 package com.carrentalservice.carrentalservice.service;
-
-import com.carrentalservice.carrentalservice.entities.Refund;
 import com.carrentalservice.carrentalservice.entities.Reservation;
-import com.carrentalservice.carrentalservice.entities.Revenue;
 import com.carrentalservice.carrentalservice.repositories.ReservationRepository;
-import com.carrentalservice.carrentalservice.repositories.RevenueRepository;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.Year;
 import java.util.List;
 
 @AllArgsConstructor
-
 @Service
 public class RevenueService {
     private final ReservationRepository reservationRepository;
@@ -38,25 +26,19 @@ public class RevenueService {
     // Fitimi për vitin aktual për një branch të caktuar
     public Double calculateCurrentYearRevenueForBranch(Long branchId) {
         List<Reservation> reservations = reservationRepository.calculateCurrentYearRevenueForBranch(branchId);
-        return reservations.stream()
-                .mapToDouble(r -> r.getAmount() + (r.getRefund() != null ? r.getRefund().getSurcharge() : 0.0))
-                .sum();
+        return reservations.stream().mapToDouble(r -> r.getAmount() + (r.getRefund() != null ? r.getRefund().getSurcharge() : 0.0)).sum();
     }
 
     // Fitimi për një branch duke filluar nga një datë e caktuar
     public Double calculateRevenueForBranchFromDate(Long branchId, LocalDateTime startDate) {
         List<Reservation> reservations = reservationRepository.calculateRevenueForBranchFromDate(branchId, startDate);
-        return reservations.stream()
-                .mapToDouble(r -> r.getAmount() + (r.getRefund() != null ? r.getRefund().getSurcharge() : 0.0))
-                .sum();
+        return reservations.stream().mapToDouble(r -> r.getAmount() + (r.getRefund() != null ? r.getRefund().getSurcharge() : 0.0)).sum();
     }
 
     // Fitimi për një rental nga një datë deri në një datë
     public Double calculateRevenueForRentalBetweenDates(Long rentalId, LocalDateTime startDate, LocalDateTime endDate) {
         List<Reservation> reservations = reservationRepository.calculateRevenueForRentalBetweenDates(rentalId, startDate, endDate);
-        return reservations.stream()
-                .mapToDouble(r -> r.getAmount() + (r.getRefund() != null ? r.getRefund().getSurcharge() : 0.0))
-                .sum();
+        return reservations.stream().mapToDouble(r -> r.getAmount() + (r.getRefund() != null ? r.getRefund().getSurcharge() : 0.0)).sum();
 
     }
 }
